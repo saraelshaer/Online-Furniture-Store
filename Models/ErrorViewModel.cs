2faa69058053422c4afccae815de79ccf5a46963
saraelshaer <saraelshaer923@gmail.com>
2024-09-15 20:02:45 -0800

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FurnitureStore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
