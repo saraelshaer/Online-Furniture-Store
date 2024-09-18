e668da3ff53affe15ccdcd75ef56aab4b6587518
SaraElazb <alzb1518@gmail.com>
2024-09-18 22:45:55 +0300

    generic repo

---
+++ IRepository/IRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace FurnitureStore.IRepository
+{
+    public interface IRepository<T> where T : class
+    {
+        IEnumerable<T> GetAll();
+        T GetById(int id);
+        void Add(T entity);
+        void Update(T entity);
+        void SoftDelete(T entity);
+        void HardDelete(T entity);
+    }
+}
---
+++ Repository/Repository.cs
@@ -0,0 +1,34 @@
+﻿using FurnitureStore.Data;
+using FurnitureStore.IRepository;
+using Microsoft.EntityFrameworkCore;
+
+namespace FurnitureStore.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly AppDbContext _context;
+        private readonly DbSet<T> _dbSet;
+
+        public Repository(AppDbContext context)
+        {
+            _context = context;
+            _dbSet = _context.Set<T>();
+        }
+        public void Add(T entity) => _dbSet.Add(entity);
+
+        public IEnumerable<T> GetAll() { return _dbSet.ToList(); }
+
+        public T GetById(int id) { return _dbSet.Find(id); }
+
+        public void Update(T entity) => _dbSet.Update(entity);
+        public void HardDelete(T entity) => _dbSet.Remove(entity);
+
+        public void SoftDelete(T entity)
+        {
+            var isActiveProp = entity.GetType().GetProperty("IsActive");
+            isActiveProp.SetValue(entity, false);
+            _dbSet.Update(entity);
+        }
+
+    }
+}
